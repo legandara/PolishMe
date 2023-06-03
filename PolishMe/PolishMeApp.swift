9b808ba051326f1d2376608247b5a24fd60e66ab
les <legandara@icloud.com>
2023-06-02 17:24:59 -0700

    Initial Commit

---
+++ PolishMe/PolishMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolishMeApp.swift
+//  PolishMe
+//
+//  Created by Le Gandara on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct PolishMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
